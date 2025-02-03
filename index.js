9586b8c59a8b098551fbb31ff7c98573aca38f5d
Steve Gelbard <stevegelbard@Steves-MacBook-Pro.local>
2025-02-03 08:09:01 +0200

    return to port 3000

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const getFunc = require('./exportsFunctions');
-const PORT = process.env.PORT || 3000;
 
 
 app.use(cors());
@@ -33,7 +32,7 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-app.listen(PORT, () => console.log('Express server is running at port no: 3000!'));
+app.listen(3000, () => console.log('Express server is running at port no: 3000!'));
 
 getFunc.getData(app, mysqlConnection);
 getFunc.searchStudent(app, mysqlConnection);
